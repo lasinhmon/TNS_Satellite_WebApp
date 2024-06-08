806cafa379427efb48aaa30804057282565f76c5
lasinhmon <130166488+lasinhmon@users.noreply.github.com>
2024-06-08 20:41:27 +0700

    Update Connecting.cs
    Security Connection

--- TNS.DBConnection/Connecting.cs
+++ TNS.DBConnection/Connecting.cs
@@ -4,13 +4,13 @@ namespace TNS.DBConnection
 {
     public class Connecting
     {
-        public static string SQL_MainDatabase = @"Data Source=171.235.24.157;DataBase=TN_Satellite;user=Satellite;Password=Tung0363675807;";
+        public static string SQL_MainDatabase = 
         //public static string SQL_MainDatabase = @"Data Source=171.235.24.157;DataBase=TN_Satellite;user=Satellite;Password=TriViet2008;";
         // public static string SQL_MainDatabase = @"Data Source=TNS_Corporation\SQLEXPRESS;DataBase=TN_Banking;user=sa;Password=123456;";
         //public static string SQL_KPI = @"Data Source=TNS_Corporation\SQLEXPRESS;DataBase=TN_KPI;user=sa;Password=TriViet;";
         //public static string SQL_MainDatabase = @"Data Source=6280-WS000149;DataBase=TN_Banking;user=WebApp;Password=TriViet1234;";
         //public static string SQL_KPI = @"Data Source=6280-WS000149;DataBase=TN_KPI;user=WebApp;Password=TriViet7808;";
         //  public static string BigQuery_Connection = @"";
-        public static BigQueryClient client = BigQueryClient.Create("loctroiproject");
+        public static BigQueryClient client = 
     }
 }
